1864a9c2da97bd2579a86f1d405d9eddf5247c1d
Andrew Nehalchuk <rtpdfv@yahoo.com>
2023-12-03 12:55:00 +0100

    qemu-aarch64-static

--- debootstrap.sh
+++ debootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+CHROOT=/path/to/chroot
 sudo debootstrap --arch amd64 bullseye /path/to/chroot http://deb.debian.org/debian/
-sudo mount -t proc none /path/to/chroot/proc
-sudo mount -t sysfs none /path/to/chroot/sys
-sudo mount -o bind /dev /path/to/chroot/dev
-sudo mount -o bind /run/udev /path/to/chroot/run/udev
+sudo mount -t proc none $CHROOT/proc
+sudo mount -t sysfs none $CHROOT/sys
+sudo mount -o bind /dev $CHROOT/dev
 #sudo cp /etc/resolv.conf /path/to/chroot/etc/resolv.conf
-sudo unshare --mount --uts --ipc --net --pid --fork --user --map-root-user chroot /path/to/chroot /bin/bash
+sudo unshare --mount --uts --ipc --net --pid --fork --user --map-root-user chroot $CHROOT /bin/bash
 
+#AARCH64
+sudo unshare --mount --uts --ipc --net --pid --fork --user --map-root-user chroot $CHROOT /qemu-aarch64-static /usr/bin/bash
 
 
 
